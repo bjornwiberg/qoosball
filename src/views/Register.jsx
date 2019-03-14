3ea17ab5a4cfffe796df70dcb6af39604387fcbf
Björn Wiberg <crx@qlik.com>
2019-03-14 14:07:56 +0100

    Add register view

---
+++ src/views/Register.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Registration from '../components/Registration';
+
+const Register = () => {
+  return (
+    <div className="register-container">
+      <Registration />
+    </div>
+  );
+}
+
+export default Register;
+
