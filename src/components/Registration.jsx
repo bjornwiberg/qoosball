1a3dfe92d1351e04fb8696cb2070c11365fd4ce5
Björn Wiberg <crx@qlik.com>
2019-03-14 15:18:35 +0100

    Add some style to the form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -3,12 +3,12 @@ import countryList from 'country-list';
 import './Registration.css';
 
 const Registration = () => {
-  const [dominantHand, updateDominantHand] = useState('');
-  const [name, updateName] = useState('');
-  const [nationality, updateNationality] = useState('');
-  const [race, updateRace] = useState('');
-  const [slackId, updateSlackId] = useState('');
-  const [trigram, updateTrigram] = useState('');
+  const [dominantHand, updateDominantHand] = useState(null);
+  const [name, updateName] = useState(null);
+  const [nationality, updateNationality] = useState(null);
+  const [race, updateRace] = useState(null);
+  const [slackId, updateSlackId] = useState(null);
+  const [trigram, updateTrigram] = useState(null);
 
   const submitForm = () => {
     if (dominantHand && name && nationality && slackId && trigram) {
@@ -35,50 +35,66 @@ const Registration = () => {
 
   return (
     <div className="registration-form">
+      <div className="registration-form__header">
+        Register to play
+      </div>
       <div className="registration-form__input-group">
-        <label for="foos-name">{fields.name} <span class="mandatory">*</span></label>
-        <input id="foos-name" type="text" onChange={e => updateName(e.target.value)} />
+        <label htmlFor="foos-name">{fields.name} <span className="mandatory">*</span></label>
+        <div className="registration-form__input-group-fields">
+          <input id="foos-name" type="text" onChange={e => updateName(e.target.value)} />
+        </div>
       </div>
       <div className="registration-form__input-group-validation">
         {validationStatus('name', name)}
       </div>
       <div className="registration-form__input-group">
-        <label for="foos-trigram">{fields.trigram} <span class="mandatory">*</span></label>
+        <label htmlFor="foos-trigram">{fields.trigram} <span className="mandatory">*</span></label>
+        <div className="registration-form__input-group-fields">
         <input id="foos-trigram" type="text" onChange={e => updateTrigram(e.target.value)} />
-        <div className="registration-form__input-group-validation">
-          {validationStatus('trigram', trigram)}
-        </div>
+      </div>
+      </div>
+      <div className="registration-form__input-group-validation">
+        {validationStatus('trigram', trigram)}
       </div>
       <div className="registration-form__input-group">
-        <label>{fields.dominantHand} <span class="mandatory">*</span></label>
-        <input type="radio" value="left" name="dominant-hand" onChange={e => updateDominantHand(e.target.value)} />
-        <input type="radio" value="right" name="dominant-hand" onChange={e => updateDominantHand(e.target.value)} />
-        <div className="registration-form__input-group-validation">
-          {validationStatus('dominantHand', dominantHand)}
+        <label>{fields.dominantHand} <span className="mandatory">*</span></label>
+        <div className="registration-form__input-group-fields">
+          <label><input type="radio" value="left" name="dominant-hand" onChange={e => updateDominantHand(e.target.value)} />Left</label>
+          <label><input type="radio" value="right" name="dominant-hand" onChange={e => updateDominantHand(e.target.value)} />Right</label>
         </div>
       </div>
-      <div className="registration-form__input-group">
-        <label for="foos-race">{fields.race}</label>
-        <input id="foos-race" type="text" onChange={e => updateRace(e.target.value)} />
+      <div className="registration-form__input-group-validation">
+        {validationStatus('dominantHand', dominantHand)}
       </div>
       <div className="registration-form__input-group">
-        <label for="foos-slack-id">{fields.slackId} <span class="mandatory">*</span></label>
-        <input id="foos-slack-id" type="text" onChange={e => updateSlackId(e.target.value)} />
-        <div className="registration-form__input-group-validation">
-          {validationStatus('slackId', slackId)}
+        <label htmlFor="foos-race">{fields.race}</label>
+        <div className="registration-form__input-group-fields">
+          <input id="foos-race" type="text" onChange={e => updateRace(e.target.value)} />
         </div>
       </div>
+      <div className="registration-form__input-group-validation" />
       <div className="registration-form__input-group">
-        <label for="foos-nationality">{fields.nationality} <span class="mandatory">*</span></label>
-        <select name="nationality" onChange={e => updateNationality(e.target.value)}>
-          <option value="">Choose a country</option>
-          {Object.entries(countryList.getCodeList()).map(([code, name ]) => (
-            <option key={code} value={code}>{name}</option>
-          ))}
-        </select>
-        <div className="registration-form__input-group-validation">
-          {validationStatus('nationality', nationality)}
+        <label htmlFor="foos-slack-id">{fields.slackId} <span className="mandatory">*</span></label>
+        <div className="registration-form__input-group-fields">
+          <input id="foos-slack-id" type="text" onChange={e => updateSlackId(e.target.value)} />
         </div>
+      </div>
+      <div className="registration-form__input-group-validation">
+        {validationStatus('slackId', slackId)}
+      </div>
+      <div className="registration-form__input-group">
+        <label htmlFor="foos-nationality">{fields.nationality} <span className="mandatory">*</span></label>
+        <div className="registration-form__input-group-fields">
+          <select name="nationality" onChange={e => updateNationality(e.target.value)}>
+            <option value="">Choose a country</option>
+            {Object.entries(countryList.getCodeList()).map(([code, name ]) => (
+              <option key={code} value={code}>{name}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="registration-form__input-group-validation">
+        {validationStatus('nationality', nationality)}
       </div>
       <div className="registration-form__button-group">
         <button onClick={submitForm}>Save</button>
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Registration from '../components/Registration';
 
+import './Register.css';
+
 const Register = () => {
   return (
     <div className="register-container">
